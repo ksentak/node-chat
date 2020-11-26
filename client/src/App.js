a81f1379100240ad2a7ac4c68f0d606c7c696b6c
KSentak <k.sentak@outlook.com>
2020-11-26 12:40:28 -0500

    add context api to help with auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 import Auth from './components/Auth';
 import Home from './components/Home';
 import Chat from './components/Chat';
 // import './App.css';
 
 const App = () => {
-	const [user, setUser] = useState(null);
+	const [{ user }, dispatch] = useStateValue();
 	return (
 		<>
 			{!user ? (
--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,12 +1,18 @@
 import { auth, provider } from '../firebase';
+import { actionTypes } from '../reducer';
+import { useStateValue } from '../StateProvider';
 import './Auth.css';
 
 const Auth = () => {
+	const [, dispatch] = useStateValue();
 	const signIn = () => {
 		auth
 			.signInWithPopup(provider)
 			.then((res) => {
-				console.log(res);
+				dispatch({
+					type: actionTypes.SET_USER,
+					user: res.user
+				});
 			})
 			.catch((err) => {
 				console.log(err.message);
